34d7c8e1995d91d828d979261163c487084676b2
daviskydriod <67622791+daviskydriod@users.noreply.github.com>
2026-05-30 01:30:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Preloader from "@/components/ui/Preloader";
-
 // Public Pages
 import Index from "./pages/Index";
 import About from "./pages/About";
@@ -16,13 +14,12 @@ import LandingPage from "./pages/LandingPage";
 import Portfolio from "./pages/Portfolio";
 import ProjectDetail from "./pages/ProjectDetail";
 import Contact from "./pages/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import NotFound from "./pages/NotFound";
-
 // Blog
 import BlogListing from "./pages/BlogListing";
 import BlogPost from "./pages/BlogPost";
 import BlogCategory from "./pages/BlogCategory";
-
 // Admin
 import Login from "./components/admin/Login";
 import Dashboard from "./components/admin/Dashboard";
@@ -42,13 +39,10 @@ const App = () => {
           {isLoading && (
             <Preloader onComplete={() => setIsLoading(false)} />
           )}
-
           <Toaster />
           <Sonner />
-
           <BrowserRouter>
             <Routes>
-
               {/* PUBLIC */}
               <Route path="/" element={<Index />} />
               <Route path="/about" element={<About />} />
@@ -57,32 +51,20 @@ const App = () => {
               <Route path="/portfolio" element={<Portfolio />} />
               <Route path="/portfolio/:slug" element={<ProjectDetail />} />
               <Route path="/contact" element={<Contact />} />
-
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
               {/* BLOG */}
               <Route path="/blog" element={<BlogListing />} />
-              <Route
-                path="/blog/category/:slug"
-                element={<BlogCategory />}
-              />
+              <Route path="/blog/category/:slug" element={<BlogCategory />} />
               <Route path="/blog/:slug" element={<BlogPost />} />
-
               {/* ADMIN */}
               <Route path="/admin/login" element={<Login />} />
               <Route path="/admin" element={<Dashboard />} />
               <Route path="/admin/posts" element={<Posts />} />
               <Route path="/admin/posts/new" element={<PostEditor />} />
-              <Route
-                path="/admin/posts/edit/:id"
-                element={<PostEditor />}
-              />
-              <Route
-                path="/admin/categories"
-                element={<Categories />}
-              />
-
+              <Route path="/admin/posts/edit/:id" element={<PostEditor />} />
+              <Route path="/admin/categories" element={<Categories />} />
               {/* 404 */}
               <Route path="*" element={<NotFound />} />
-
             </Routes>
           </BrowserRouter>
         </TooltipProvider>
